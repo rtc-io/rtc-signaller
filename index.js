ee53c764c18db4aee080407448fae96f231c9dca
Damon Oehlman <damon.oehlman@gmail.com>
2013-06-18 13:59:42 +1000

    Moving channel naming logic to signaller from transport

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 var EventEmitter = require('events').EventEmitter,
-	pull = require('pull-stream'),
-	util = require('util'),
+    pull = require('pull-stream'),
+    util = require('util'),
     uuid = require('uuid'),
-	knownTransports = {};
+    knownTransports = {};
 
 /**
 # SignallingChannel()
 */
 function SignallingChannel(opts) {
-	if (! (this instanceof SignallingChannel)) return new SignallingChannel(opts);
+    if (! (this instanceof SignallingChannel)) return new SignallingChannel(opts);
 
-	// init as a duplex stream
-	EventEmitter.call(this);
+    // init as a duplex stream
+    EventEmitter.call(this);
 
-	// if opts is a string, then we have a channel name
-	if (typeof opts == 'string' || (opts instanceof String)) {
-		opts = {
-			channel: opts
-		};
-	}
+    // if opts is a string, then we have a channel name
+    if (typeof opts == 'string' || (opts instanceof String)) {
+        opts = {
+            channel: opts
+        };
+    }
 
-	// ensure we have an opts hash
-	opts = opts || {};
+    // ensure we have an opts hash
+    opts = opts || {};
 
     // initialise the channel name
     // TODO: investigate generating shorter unique channel names (uuids aren't easy to communicate)
     this.name = opts.channel || uuid.v4();
 
-	// initialise the messages queue
-	this.messages = require('pull-pushable');
+    // initialise the messages queue
+    this.messages = require('pull-pushable');
 
-	// initialise members
-	this.peers = [];
-	this._transport = null;
+    // initialise members
+    this.peers = [];
+    this._transport = null;
 }
 
 util.inherits(SignallingChannel, EventEmitter);
@@ -42,60 +42,73 @@ module.exports = SignallingChannel;
 ## add(peer)
 */
 SignallingChannel.prototype.add = function(peer) {
-	if (! (peer instanceof RTCPeerConnection)) return;
+    if (! (peer instanceof RTCPeerConnection)) return;
 
-	// add the peer to the active peers list
-	this.peers.push(peer);
+    // add the peer to the active peers list
+    this.peers.push(peer);
 
-	// TODO: connect to the relevant RTCPeerConnection events and respond accordingly
-	peer.addEventListener('negotiationneeded', this._negotiate.bind(this, peer));
+    // TODO: connect to the relevant RTCPeerConnection events and respond accordingly
+    peer.addEventListener('negotiationneeded', this._negotiate.bind(this, peer));
 };
 
 /**
 ## remove(peer)
 */
 SignallingChannel.prototype.remove = function(peer) {
-	var index = this.peers.indexOf(peer);
+    var index = this.peers.indexOf(peer);
 
-	// if we are managing the peer, then remove event listeners
-	if (index >= 0) {
-		// TODO: unbind event listeners
+    // if we are managing the peer, then remove event listeners
+    if (index >= 0) {
+        // TODO: unbind event listeners
 
-		// remove the peer from the list
-		this.peers.splice(index, 1);		
-	}
+        // remove the peer from the list
+        this.peers.splice(index, 1);        
+    }
 };
 
 /**
 ## transport
 */
 Object.defineProperty(SignallingChannel.prototype, 'transport', {
-	get: function() {
-		return this._transport;
-	},
+    get: function() {
+        return this._transport;
+    },
 
-	set: function(transport) {
-		// if this is the same transport, do nothing
-		if (this._transport === transport) return;
+    set: function(transport) {
+        var channel = this;
 
-		// if we have an existing transport, then disconnect
-		if (this._transport) {
+        // if this is the same transport, do nothing
+        if (this._transport === transport) return;
 
-		}
+        // if we have an existing transport, then disconnect
+        if (this._transport) {
 
-		// update the transport
-		this._transport = transport;
+        }
 
-        // set the channel name in the transport
-        transport.setChannelName(this.name);
+        // if the transport does not have an init function emit an error
+        if (typeof transport.connect != 'function') {
+            return this.emit('error', new Error('Cannot initialize transport, ensure transport has a connect method'));
+        }
 
-		// push the messages to the transport
-		// this.messages.pipe(pull.drain(transport.write.bind(transport)));
+        // update the transport
+        this._transport = transport;
 
-		// listen for messages from the transport
-		pull(
-			transport.createReader(),
-			pull.drain(this.emit.bind(this, 'message'))
-		);
-	}
+        // connect the transport
+        transport.connect(function(err) {
+            if (err) return channel.emit('error', err);
+
+            // pipe signaller messages to the transport
+            channel.messages.pipe(pull.drain(transport.createWriter()));
+
+            // listen for messages from the transport and emit them as messages
+            pull(
+                transport.createReader(),
+                pull.drain(function(data) {
+                    channel.emit('message', data);
+                })
+            );
+
+            channel.messages.push('/channel ' + channel.name);            
+        });
+    }
 })
