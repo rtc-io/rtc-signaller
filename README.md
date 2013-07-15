8133ad03fcc68e0bf2123f992e81ac5da0d833c0
Damon Oehlman <damon.oehlman@gmail.com>
2013-07-15 14:36:53 +1000

    Updated code to comply with code style

--- README.md
+++ README.md
@@ -1,23 +1,38 @@
 # rtc-signaller
 
-[![Build Status](http://etd-packaging.research.nicta.com.au/jenkins/view/rtc/job/rtc-signaller/badge/icon)](http://etd-packaging.research.nicta.com.au/jenkins/view/rtc/job/rtc-signaller/)
+[!
+[Build Status]
+(http://etd-packaging.research.nicta.com.au/jenkins/view/rtc/job/
+rtc-signaller/badge/icon)
+]
+(http://etd-packaging.research.nicta.com.au/jenkins/view/rtc/job/
+rtc-signaller/)
 
-The `rtc-signaller` package provides a simple interface for WebRTC Signalling that is protocol independent.  Rather than tie the implementation specifically to Websockets, XHR, etc. the signaller package allows you to implement signalling in your application and then `pipe` it to the appropriate output interface.
+The `rtc-signaller` package provides a simple interface for WebRTC Signalling
+that is protocol independent.  Rather than tie the implementation
+specifically to Websockets, XHR, etc. the signaller package allows you to
+implement signalling in your application and then `pipe` it to the
+appropriate output interface.
 
-This in turn reduces the overall effort required to implement WebRTC signalling over different protocols and also means that your application code is able to include different underlying transports with relative ease.
+This in turn reduces the overall effort required to implement WebRTC
+signalling over different protocols and also means that your application code
+is able to include different underlying transports with relative ease.
 
 ## Getting Started (Client)
 
 ### Creating a Signaller and Associating a Transport
 
-The first thing you will need to do is to include the `rtc-signaller` package in your application, and provide it a channel name that it will use to communicate with its peers.
+The first thing you will need to do is to include the `rtc-signaller`package
+in your application, and provide it a channel name that it will use to
+communicate with its peers.
 
 ```js
 var signaller = require('rtc-signaller'),
-	channel = signaller('channel-name');
+channel = signaller('channel-name');
 ```
 
-The next thing to do is to tell the signaller which transport it is going to be using:
+The next thing to do is to tell the signaller which transport it is
+going to be using:
 
 ```js
 channel.setTransport(require('rtc-signaller-socket.io')({ host: 'rtc.io' }));
@@ -25,51 +40,119 @@ channel.setTransport(require('rtc-signaller-socket.io')({ host: 'rtc.io' }));
 
 ### Handling Events
 
-At the point at which the transport is associated, the channel will attempt to connect. Once this connection is established, the signaller will become aware of other peers (if any) that are currently connected to the room:
+At the point at which the transport is associated, the channel will attempt
+to connect. Once this connection is established, the signaller will become
+aware of other peers (if any) that are currently connected to the room:
 
 ```js
 // wait for ready event
 channel.once('ready', function() {
-	console.log('connected to ' + channel.name + ' with ' + channel.peers.length + ' other peers');
+console.log('connected to ' + channel.name);
 });
 ```
 
-In addition to the `ready` event, the channel will also trigger a `peer:discover` event to signal the discovery of a new peer in the channel:
+In addition to the `ready` event, the channel will also trigger a
+`peer:discover` event to signal the discovery of a new peer in the channel:
 
 ```js
 channel.on('peer:discover', function(peer) {
-	console.log('discovered a new peer in the channel');
+console.log('discovered a new peer in the channel');
 });
 ```
 
 ### Peers vs Peer Connections
 
-When working with WebRTC and signalling concepts, it's important to differentiate between peers and actual `RTCPeerConnection` instances.  From a signalling perspective, a peer is someone (or something) out there that we can potentially connect with.
+When working with WebRTC and signalling concepts, it's important to
+differentiate between peers and actual `RTCPeerConnection` instances.
+From a signalling perspective, a peer is someone (or something) out there
+that we can potentially connect with.
 
-It is completely reasonable to have 0..n `RTCPeerConnection` instances associated with each of the peers that we have knowledge of.
+It is completely reasonable to have 0..n `RTCPeerConnection` instances
+associated with each of the peers that we have knowledge of.
 
 ### Connecting to a Peer
 
-So once we know about other peers in the current channel, we should probably look at how we can start connecting with them.  To do this we need to start the offer-answer negotiation process.
+So once we know about other peers in the current channel, we should probably
+look at how we can start connecting with them.  To do this we need to start
+the offer-answer negotiation process.
 
-In an application where we want to automatically connect to every other known peer in the channel, we could implement code similar to that shown below:
+In an application where we want to automatically connect to every other
+known peer in the channel, we could implement code similar to that shown
+below:
 
 ```js
 channel.on('peer:discover', function(peer) {
-	var connection = channel.connect(peer);
+var connection = channel.connect(peer);
 
-	// add a stream to the connection
-	connection.addStream(media.stream);
+// add a stream to the connection
+connection.addStream(media.stream);
 });
 ```
 
-In the example above, once a peer is discovered our application will automatically attempt to connect with the peer by [creating an offer](http://dev.w3.org/2011/webrtc/editor/webrtc.html#widl-RTCPeerConnection-createOffer-void-RTCSessionDescriptionCallback-successCallback-RTCPeerConnectionErrorCallback-failureCallback-MediaConstraints-constraints) and then sending that offer using the signaller transport.
+In the example above, once a peer is discovered our application will
+automatically attempt to connect with the peer by
+[creating an offer]
+(http://dev.w3.org/2011/webrtc/editor/webrtc.html
+#widl-RTCPeerConnection-createOffer-void-RTCSessionDescriptionCallback
+-successCallback-RTCPeerConnectionErrorCallback-
+failureCallback-MediaConstraints-constraints) and then sending that offer
+using the signaller transport.
 
-Now in most circumstances using code like the sample above will result in two peers creating offers for each other and duplicating the connection requests.  In this situation, the [signaller will coordinate the connection requests](http://git-ent.research.nicta.com.au/doehlman/rtc-signaller/issues/1) and make sure that peers find each other correctly.
+Now in most circumstances using code like the sample above will result in
+two peers creating offers for each other and duplicating the connection
+requests.  In this situation, the
+[signaller will coordinate the connection requests]
+(http://git-ent.research.nicta.com.au/doehlman/rtc-signaller/issues/1)
+and make sure that peers find each other correctly.
 
-Once two peers have established a working connection, the channel will let you know:
+Once two peers have established a working connection, the channel
+will let you know:
 
 ```js
 channel.on('peer:connect', function(connection) {
 });
 ```
+
+## Signaller prototype
+
+An instance of a Signaller prototype supports the following methods:
+
+### connect(callback)
+
+### inbound()
+
+Return a pull-stream sink for messages generated by the transport
+
+### join(name, callback)
+
+Send a join command to the signalling server, indicating that you would like 
+to join the current room.  In the current implementation of the rtc.io suite
+it is only possible for the signalling client to exist in one room at one
+particular time, so joining a new channel will automatically mean leaving the
+existing one if already joined.
+
+### negotiate(targetId, sdp, callId, type)
+
+The negotiate function handles the process of sending an updated Session
+Description Protocol (SDP) description to the specified target signalling
+peer.  If this is an established connection, then a callId will be used to 
+ensure the sdp is deliver to the correct RTCPeerConnection instance
+
+### outbound()
+
+Return a pull-stream source that can write messages to a transport
+
+### send(data)
+
+Send data across the line
+
+### _autoConnect(opts)
+
+## Helper Functions (Not Exported)
+
+### createMessageParser(signaller)
+
+This is used to create a function handler that will operate more quickly that 
+when using bind.  The parser will pull apart a message into parts (splitting 
+on the pipe character), parsing those parts where appropriate and then
+triggering the relevant event.
--- index.js
+++ index.js
@@ -1,212 +1,341 @@
-var EventEmitter = require('events').EventEmitter,
-    pull = require('pull-stream'),
-    pushable = require('pull-pushable'),
-    util = require('util'),
-    errorcodes = require('rtc-core/errorcodes');
+/*jshint node:true*/
+
+'use strict';
+
+var EventEmitter = require('events').EventEmitter;
+var pull = require('pull-stream');
+var pushable = require('pull-pushable');
+var util = require('util');
 
 /**
-# Signaller()
-*/
+# rtc-signaller
+
+[!
+  [Build Status]
+  (http://etd-packaging.research.nicta.com.au/jenkins/view/rtc/job/
+  rtc-signaller/badge/icon)
+]
+(http://etd-packaging.research.nicta.com.au/jenkins/view/rtc/job/
+rtc-signaller/)
+
+The `rtc-signaller` package provides a simple interface for WebRTC Signalling
+that is protocol independent.  Rather than tie the implementation
+specifically to Websockets, XHR, etc. the signaller package allows you to
+implement signalling in your application and then `pipe` it to the
+appropriate output interface.
+
+This in turn reduces the overall effort required to implement WebRTC
+signalling over different protocols and also means that your application code
+is able to include different underlying transports with relative ease.
+
+## Getting Started (Client)
+
+### Creating a Signaller and Associating a Transport
+
+The first thing you will need to do is to include the `rtc-signaller`package
+in your application, and provide it a channel name that it will use to
+communicate with its peers.
+
+```js
+var signaller = require('rtc-signaller'),
+  channel = signaller('channel-name');
+```
+
+The next thing to do is to tell the signaller which transport it is
+going to be using:
+
+```js
+channel.setTransport(require('rtc-signaller-socket.io')({ host: 'rtc.io' }));
+```
+
+### Handling Events
+
+At the point at which the transport is associated, the channel will attempt
+to connect. Once this connection is established, the signaller will become
+aware of other peers (if any) that are currently connected to the room:
+
+```js
+// wait for ready event
+channel.once('ready', function() {
+  console.log('connected to ' + channel.name);
+});
+```
+
+In addition to the `ready` event, the channel will also trigger a
+`peer:discover` event to signal the discovery of a new peer in the channel:
+
+```js
+channel.on('peer:discover', function(peer) {
+  console.log('discovered a new peer in the channel');
+});
+```
+
+### Peers vs Peer Connections
+
+When working with WebRTC and signalling concepts, it's important to
+differentiate between peers and actual `RTCPeerConnection` instances.
+From a signalling perspective, a peer is someone (or something) out there
+that we can potentially connect with.
+
+It is completely reasonable to have 0..n `RTCPeerConnection` instances
+associated with each of the peers that we have knowledge of.
+
+### Connecting to a Peer
+
+So once we know about other peers in the current channel, we should probably
+look at how we can start connecting with them.  To do this we need to start
+the offer-answer negotiation process.
+
+In an application where we want to automatically connect to every other
+known peer in the channel, we could implement code similar to that shown
+below:
+
+```js
+channel.on('peer:discover', function(peer) {
+  var connection = channel.connect(peer);
+
+  // add a stream to the connection
+  connection.addStream(media.stream);
+});
+```
+
+In the example above, once a peer is discovered our application will
+automatically attempt to connect with the peer by
+[creating an offer]
+(http://dev.w3.org/2011/webrtc/editor/webrtc.html
+#widl-RTCPeerConnection-createOffer-void-RTCSessionDescriptionCallback
+-successCallback-RTCPeerConnectionErrorCallback-
+failureCallback-MediaConstraints-constraints) and then sending that offer
+using the signaller transport.
+
+Now in most circumstances using code like the sample above will result in
+two peers creating offers for each other and duplicating the connection
+requests.  In this situation, the
+[signaller will coordinate the connection requests]
+(http://git-ent.research.nicta.com.au/doehlman/rtc-signaller/issues/1)
+and make sure that peers find each other correctly.
+
+Once two peers have established a working connection, the channel
+will let you know:
+
+```js
+channel.on('peer:connect', function(connection) {
+});
+```
+**/
+
+/**
+## Signaller prototype
+
+An instance of a Signaller prototype supports the following methods:
+**/
 function Signaller(opts) {
-    if (! (this instanceof Signaller)) return new Signaller(opts);
-    EventEmitter.call(this);
+  if (! (this instanceof Signaller)) {
+    return new Signaller(opts);
+  }
 
-    // if opts is a string, then we have a channel name
-    if (typeof opts == 'string' || (opts instanceof String)) {
-        opts = {
-            channel: opts
-        };
-    }
+  EventEmitter.call(this);
 
-    // initialise the messages queue
-    this._outboundMessages = pushable();
+  // if opts is a string, then we have a channel name
+  if (typeof opts == 'string' || (opts instanceof String)) {
+    opts = {
+      channel: opts
+    };
+  }
 
-    // ensure we have an opts hash
-    this.opts = opts = opts || {};
+  // initialise the messages queue
+  this._outboundMessages = pushable();
 
-    // ensure we have a transport creator
-    this.transport = opts.transport || 
-        opts.transportCreator || 
-        require('./transports/socket');
+  // ensure we have an opts hash
+  this.opts = opts = opts || {};
 
-    // initialise members
-    this.debug = opts.debug && typeof console.log == 'function';
+  // ensure we have a transport creator
+  this.transport = opts.transport ||
+    opts.transportCreator ||
+    require('./transports/socket');
 
-    // initialise the channel name
-    this.channel = '';
+  // initialise members
+  this.debug = opts.debug && typeof console.log == 'function';
 
-    // if the autoconnect option is not false, and we have a transport
-    // connect on next tick
-    if (typeof opts.autoConnect == 'undefined' || opts.autoConnect) {
-        process.nextTick(this._autoConnect.bind(this, opts));
-    }
+  // initialise the channel name
+  this.channel = '';
+
+  // if the autoconnect option is not false, and we have a transport
+  // connect on next tick
+  if (typeof opts.autoConnect == 'undefined' || opts.autoConnect) {
+    process.nextTick(this._autoConnect.bind(this, opts));
+  }
 }
 
 util.inherits(Signaller, EventEmitter);
 module.exports = Signaller;
 
 /**
-## connect(callback)
-*/
+### connect(callback)
+**/
 Signaller.prototype.connect = function(callback) {
-    var signaller = this,
-        peer;
+  var signaller = this;
+  var proxy;
 
-    // create a default callback
-    // TODO: make the default callback useful
-    callback = callback || function() {};
+  // create a default callback
+  // TODO: make the default callback useful
+  callback = callback || function() {};
 
-    // if we don't have a transport, return an error
-    if (! this.transport) {
-        return callback(new Error('A transport is required to connect'));
-    }
+  // if we don't have a transport, return an error
+  if (! this.transport) {
+    return callback(new Error('A transport is required to connect'));
+  }
 
-    // when we receive the connect:ok event trigger the callback
-    this.once('connect:ok', function(id) {
-        // update the signaller id
-        signaller.id = id;
+  // when we receive the connect:ok event trigger the callback
+  this.once('connect:ok', function(id) {
+    // update the signaller id
+    signaller.id = id;
 
-        // trigger the callback
-        callback(null, id);
-    });
+    // trigger the callback
+    callback(null, id);
+  });
 
-    // create the new peer proxy
-    proxy = this.transport(this.opts);
+  // create the new peer proxy
+  proxy = this.transport(this.opts);
 
-    // pipe signaller messages to the peer proxy
-    signaller.outbound().pipe(proxy.inbound());
+  // pipe signaller messages to the peer proxy
+  signaller.outbound().pipe(proxy.inbound());
 
-    // listen for messages from the peer proxy and parse those messages
-    proxy.outbound().pipe(signaller.inbound());
+  // listen for messages from the peer proxy and parse those messages
+  proxy.outbound().pipe(signaller.inbound());
 };
 
 /**
-## inbound()
+### inbound()
 
 Return a pull-stream sink for messages generated by the transport
-*/
+**/
 Signaller.prototype.inbound = function() {
-    return pull.drain(createMessageParser(this));
+  return pull.drain(createMessageParser(this));
 };
 
 /**
-## join(name, callback)
+### join(name, callback)
 
 Send a join command to the signalling server, indicating that you would like 
 to join the current room.  In the current implementation of the rtc.io suite
 it is only possible for the signalling client to exist in one room at one
 particular time, so joining a new channel will automatically mean leaving the
 existing one if already joined.
-*/
+**/
 Signaller.prototype.join = function(name, callback) {
-    var signaller = this;
+  var signaller = this;
 
-    // handle the pre:join:ok handler and update the channel name
-    this.once('pre:join:ok', function(newChannel) {
-        signaller.channel = newChannel;
-    });
+  // handle the pre:join:ok handler and update the channel name
+  this.once('pre:join:ok', function(newChannel) {
+    signaller.channel = newChannel;
+  });
 
-    if (callback) {
-        this.once('join:ok', callback);
-    }
+  if (callback) {
+    this.once('join:ok', callback);
+  }
 
-    return this.send('/join', name);
+  return this.send('/join', name);
 };
 
 /**
-## negotiate(targetId, sdp, callId, type)
+### negotiate(targetId, sdp, callId, type)
 
 The negotiate function handles the process of sending an updated Session
 Description Protocol (SDP) description to the specified target signalling
 peer.  If this is an established connection, then a callId will be used to 
 ensure the sdp is deliver to the correct RTCPeerConnection instance
-*/
+**/
 Signaller.prototype.negotiate = function(targetId, sdp, callId, type) {
-    return this.send('/negotiate', targetId, sdp, callId || '', type);
+  return this.send('/negotiate', targetId, sdp, callId || '', type);
 };
 
 /**
-## outbound()
+### outbound()
 
 Return a pull-stream source that can write messages to a transport
-*/
+**/
 Signaller.prototype.outbound = function() {
-    return this._outboundMessages;
+  return this._outboundMessages;
 };
 
 /**
-## send(data)
+### send(data)
 
 Send data across the line
-*/
+**/
 Signaller.prototype.send = function() {
-    // get the args and jsonify as required
-    var data = [].slice.call(arguments).map(function(arg) {
-            return typeof arg == 'object' ? JSON.stringify(arg) : arg;
-        }).join('|');
+  // get the args and jsonify as required
+  var data = [].slice.call(arguments).map(function(arg) {
+    return typeof arg == 'object' ? JSON.stringify(arg) : arg;
+  }).join('|');
 
-    if (this.debug) {
-        console.log('--> ' + data);
-    }
+  if (this.debug) {
+    console.log('--> ' + data);
+  }
 
-    this._outboundMessages.push(data);
+  this._outboundMessages.push(data);
 
-    // chainable
-    return this;
+  // chainable
+  return this;
 };
 
 /* internals */
 
 /**
-## _autoConnect(opts)
-*/
+### _autoConnect(opts)
+**/
 Signaller.prototype._autoConnect = function(opts) {
-    // if we have no transport, abort
-    if (! this.transport) return;
+  // if we have no transport, abort
+  if (! this.transport) { return; }
 
-    // connect
-    this.connect();
+  // connect
+  this.connect();
 
-    // if a channel has been specified, then update the channel name
-    if (opts.channel) {
-        this.join(opts.channel);
-    }
+  // if a channel has been specified, then update the channel name
+  if (opts.channel) {
+    this.join(opts.channel);
+  }
 };
 
 /**
-## createMessageParser(signaller)
+## Helper Functions (Not Exported)
+**/
+
+/**
+### createMessageParser(signaller)
 
 This is used to create a function handler that will operate more quickly that 
 when using bind.  The parser will pull apart a message into parts (splitting 
 on the pipe character), parsing those parts where appropriate and then
 triggering the relevant event.
-*/
+**/
 function createMessageParser(signaller) {
-    return function(data) {
-        var parts = (data || '').split('|'),
-            evtName = (parts[0] || '').toLowerCase(),
-            args = parts.slice(1).map(function(arg) {
-                // if it looks like JSON then parse it
-                return ['{', '['].indexOf(arg.charAt(0)) >= 0 ? JSON.parse(arg) : arg;
-            });
+  return function(data) {
+    var parts = (data || '').split('|');
+    var evtName = (parts[0] || '').toLowerCase();
+    var args = parts.slice(1).map(function(arg) {
+      // if it looks like JSON then parse it
+      return ['{', '['].indexOf(arg.charAt(0)) >= 0 ?JSON.parse(arg) : arg;
+    });
 
-        if (signaller.debug) {
-            console.log('<-- ' + data);
-        }
+    if (signaller.debug) {
+      console.log('<-- ' + data);
+    }
 
-        // trigger the event
-        if (evtName) {
-            // trigger the message pre-processor
-            signaller.emit.apply(signaller, ['pre:' + evtName].concat(args));
+    // trigger the event
+    if (evtName) {
+      // trigger the message pre-processor
+      signaller.emit.apply(signaller, ['pre:' + evtName].concat(args));
 
-            // trigger the main processor
-            if (! signaller.emit.apply(signaller, [evtName].concat(args))) {
-                // if not handled by a specific message parser then emit
-                // as a raw message for something else to handle (potentially)
-                signaller.emit('message', data);
-            }
+      // trigger the main processor
+      if (! signaller.emit.apply(signaller, [evtName].concat(args))) {
+        // if not handled by a specific message parser then emit
+        // as a raw message for something else to handle (potentially)
+        signaller.emit('message', data);
+      }
 
-            // TODO: emit a data message for the 
-        }
-    };
+      // TODO: emit a data message for the 
+    }
+  };
 }
