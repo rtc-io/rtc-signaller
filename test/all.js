bc71a8a7b71f72eb9ad8aaa19dc28541a3eeef29
Damon Oehlman <damon.oehlman@gmail.com>
2013-12-05 13:36:07 +1100

    Removed request and channel tests

--- test/all.js
+++ test/all.js
@@ -3,11 +3,7 @@ var messenger = peers.shift();
 
 // test signalling logic
 require('./announce')(messenger, peers);
-require('./request')(messenger, peers);
 require('./to')(messenger, peers);
-
-// run the channel test with 3 new peers
-require('./channel')(require('./helpers/createPeers')(3));
 
 if (typeof window != 'undefined') {
   // test primus loading
