32e515f93ca43165af42139df1f533fc3dbc275f
Damon Oehlman <damon.oehlman@gmail.com>
2014-04-01 15:58:58 +1100

    Started adding tests for pure browser WebSockets

---
+++ test/browser-websockets.js
@@ -0,0 +1,42 @@
+var test = require('tape');
+var signaller = require('..');
+var url = require('url');
+var parts = url.parse(location.origin);
+var wsUrl = 'ws' + parts.protocol.slice(4) + '//' + parts.host + '/primus';
+var socket;
+var sig;
+
+test('can create a new websocket connection to test server', function(t) {
+  t.plan(1);
+  socket = new WebSocket(wsUrl);
+  t.ok(socket instanceof WebSocket, 'websocket created');
+});
+
+test('socket opens successfully', function(t) {
+  t.plan(1);
+  socket.onopen = t.pass.bind(t, 'opened');
+});
+
+test('close the socket', function(t) {
+  t.plan(1);
+  socket.close();
+  t.pass('socket closed');
+});
+
+test('create a new socket', function(t) {
+  t.plan(1);
+  socket = new WebSocket(wsUrl);
+  t.ok(socket instanceof WebSocket, 'websocket created');
+});
+
+test('create a new signaller wrapping the socket, wait for open', function(t) {
+  t.plan(1);
+  sig = signaller(socket);
+  sig.once('open', t.pass.bind(t, 'signaller ready'));
+});
+
+test('close the socket', function(t) {
+  t.plan(1);
+  socket.close();
+  t.pass('socket closed');
+});
