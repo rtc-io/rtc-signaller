ccd441187600f8f2a913df09b529453949abde06
Damon Oehlman <damon.oehlman@gmail.com>
2014-04-03 21:15:16 +1100

    Look for peer:disconnected events

--- examples/getting-started.js
+++ examples/getting-started.js
@@ -6,6 +6,10 @@ signaller.on('peer:announce', function(data) {
  console.log('new peer found in room: ', data);
 });
 
+signaller.on('peer:disconnected', function(id) {
+  console.log('peer ' + id + ' has been disconnected');
+});
+
 // when a peer leaves the switchboard, log it
 signaller.on('peer:leave', function(id) {
   console.log('peer ' + id + ' has left the room');
