3dd9e952145c30fab98e53b309e4b15a8253bca3
Damon Oehlman <damon.oehlman@gmail.com>
2014-04-23 14:22:34 +1000

    Add basic announce tests using switchboard signalling

---
+++ test/primus-announce-payload.js
@@ -0,0 +1,46 @@
+var test = require('tape');
+var roomId = require('uuid').v4();
+var jsonparse = require('cog/jsonparse');
+var signaller;
+var signallingServer = location.origin;
+// var signallingServer = 'http://rtc.io/switchboard/';
+
+test('create a signaller', function(t) {
+  t.plan(2);
+  t.ok(signaller = require('../')(signallingServer), 'created');
+  signaller.once('init', t.pass.bind(t, 'initialized'));
+});
+
+test('announce results in a single send to the server', function(t) {
+  var spark = signaller._messenger;
+  var announceRemaining = 1;
+
+  function handleData(data) {
+    var parts = data.split('|').map(jsonparse);
+
+    // if we have an announce message, decrement
+    if (parts[0] === '/announce') {
+      if (announceRemaining <= 0) {
+        t.fail('sending more than one announce message');
+      }
+
+      announceRemaining -= 1;
+      t.ok(
+        parts[1].id === signaller.id &&
+        parts[2].id === signaller.id,
+        'announce message valid'
+      );
+    }
+  }
+
+  t.plan(3);
+  t.equal(typeof spark, 'object', 'primus initialized');
+  spark.on('outgoing::data', handleData);
+
+  setTimeout(function() {
+    spark.removeListener('outgoing::data', handleData);
+    t.equal(announceRemaining, 0, 'announce sent ok');
+  }, 1000);
+
+  signaller.announce({ name: 'Fred' });
+});
--- test/primus-announce.js
+++ test/primus-announce.js
@@ -1,46 +1,75 @@
 var test = require('tape');
-var roomId = require('uuid').v4();
-var jsonparse = require('cog/jsonparse');
-var signaller;
+var messenger = require('messenger-memory');
+var signaller = require('..');
+var uuid = require('uuid');
+var scope = [];
+var signallers = [];
+
 var signallingServer = location.origin;
 // var signallingServer = 'http://rtc.io/switchboard/';
 
-test('create a signaller', function(t) {
+test('create signaller:0', function(t) {
   t.plan(2);
-  t.ok(signaller = require('../')(signallingServer), 'created');
-  signaller.once('init', t.pass.bind(t, 'initialized'));
+  t.ok(signallers[0] = require('../')(signallingServer), 'created');
+  signallers[0].once('init', t.pass.bind(t, 'initialized'));
 });
 
-test('announce results in a single send to the server', function(t) {
-  var spark = signaller._messenger;
-  var announceRemaining = 1;
+test('create signaller:1', function(t) {
+  t.plan(2);
+  t.ok(signallers[1] = require('../')(signallingServer), 'created');
+  signallers[1].once('init', t.pass.bind(t, 'initialized'));
+});
 
-  function handleData(data) {
-    var parts = data.split('|').map(jsonparse);
+test('concurrent announce', function(t) {
+  t.plan(5);
 
-    // if we have an announce message, decrement
-    if (parts[0] === '/announce') {
-      if (announceRemaining <= 0) {
-        t.fail('sending more than one announce message');
-      }
+  signallers[1].on('peer:announce', function(data) {
+    t.equal(data.name, 'Fred', 'signaller 0 announce captured by signaller 1');
+    t.ok(signallers[1].peers.get(data.id), 'signaller 1 has noted relationship with signaller 0');
+  });
 
-      announceRemaining -= 1;
-      t.ok(
-        parts[1].id === signaller.id &&
-        parts[2].id === signaller.id,
-        'announce message valid'
-      );
-    }
-  }
-
-  t.plan(3);
-  t.equal(typeof spark, 'object', 'primus initialized');
-  spark.on('outgoing::data', handleData);
+  signallers[0].on('peer:announce', function(data) {
+    // once peer:1 has processed peer:0 announce it will respond
+    // if it is a new peer
+    t.equal(data.id, signallers[1].id, 'signaller 1 has announced itself in response');
+    t.ok(signallers[0].peers.get(data.id), 'signaller 0 has noted relationship with signaller 1');
+  });
 
   setTimeout(function() {
-    spark.removeListener('outgoing::data', handleData);
-    t.equal(announceRemaining, 0, 'announce sent ok');
+    signallers[0].removeAllListeners();
+    signallers[1].removeAllListeners();
+    t.pass('received only the 1 announce message for each peer');
   }, 1000);
 
-  signaller.announce({ name: 'Fred' });
+  // peer 0 initiates the announce process
+  signallers[0].announce({ name: 'Fred' });
+  signallers[1].announce({ name: 'Bob' });
+});
+
+test('ab roles have been correctly assigned', function(t) {
+  var data0;
+  var data1;
+
+  t.plan(4);
+  t.ok(data0 = signallers[1].peers.get(signallers[0].id), 'got data for peer 0');
+  t.ok(data1 = signallers[0].peers.get(signallers[1].id), 'got data for peer 1');
+
+  // ensure that data0 and data1 have inverse relationships for local and remote
+  t.equal(data0.remote, data1.local, 'data 0 remote === data 1 local');
+  t.equal(data1.local, data0.remote, 'data 1 local === data 0 remote');
+});
+
+test('isMaster checks are accurate', function(t) {
+  var alphaId = [signallers[0].id, signallers[1].id].sort()[0];
+
+  t.plan(2);
+
+  if (alphaId === signallers[0].id) {
+    t.ok(signallers[0].isMaster(signallers[1].id));
+    t.notOk(signallers[1].isMaster(signallers[0].id));
+  }
+  else {
+    t.notOk(signallers[0].isMaster(signallers[1].id));
+    t.ok(signallers[1].isMaster(signallers[0].id));
+  }
 });
