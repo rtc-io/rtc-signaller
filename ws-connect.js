ccfee570726221615b873b84e9afd3ce0221c3d5
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-10 16:31:27 +1000

    Added pure ws connector for full node compat for signaller

---
+++ ws-connect.js
@@ -0,0 +1,28 @@
+var WebSocket = require('ws');
+var reHttpSignalhost = /^http(.*)$/;
+var reTrailingSlash = /\/$/;
+
+function connect(signalhost) {
+  // if we have a http/https signalhost then do some replacement magic to push across
+  // to ws implementation (also add the /primus endpoint)
+  if (reHttpSignalhost.test(signalhost)) {
+    signalhost = signalhost
+      .replace(reHttpSignalhost, 'ws$1')
+      .replace(reTrailingSlash, '') + '/primus';
+  }
+
+  return new WebSocket(signalhost);
+}
+
+module.exports = function(signalhost, callback) {
+  var ws = connect(signalhost);
+
+  ws.once('open', function() {
+    // close the test socket
+    ws.close();
+
+    callback(null, {
+      connect: connect
+    });
+  });
+};
