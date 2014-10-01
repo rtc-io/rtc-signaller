88e27453e4d1be069ef27a5aa1a8691a12c6be78
Damon Oehlman <damon.oehlman@gmail.com>
2014-10-01 14:14:05 +1000

    Configure the node layer to expect ws event names

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var extend = require('cog/extend');
 
 module.exports = function(messenger, opts) {
   return require('./index.js')(messenger, extend({
+    writeMethod: 'send',
+    dataEvent: 'message',
     connect: require('./ws-connect')
   }, opts));
 };
