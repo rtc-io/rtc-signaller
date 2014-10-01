df52dedb277004b73a46c342ea3cf790c5c87e82
Damon Oehlman <damon.oehlman@gmail.com>
2014-10-01 14:46:32 +1000

    Use signaller.js instead of index.js

--- browser.js
+++ browser.js
@@ -1,7 +1,7 @@
 var extend = require('cog/extend');
 
 module.exports = function(messenger, opts) {
-  return require('./index.js')(messenger, extend({
+  return require('./signaller.js')(messenger, extend({
     connect: require('./primus-loader')
   }, opts));
 };
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var extend = require('cog/extend');
 
 module.exports = function(messenger, opts) {
-  return require('./index.js')(messenger, extend({
+  return require('./signaller.js')(messenger, extend({
     writeMethod: 'send',
     dataEvent: 'message',
     connect: require('./ws-connect')
