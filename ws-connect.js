49cb1ee49686ea320689ae97ce94c5d593cccdbb
Damon Oehlman <damon.oehlman@gmail.com>
2014-10-01 13:09:01 +1000

    Improve node socket connection to ping as per what a primus server expects

--- ws-connect.js
+++ ws-connect.js
@@ -1,8 +1,13 @@
 var WebSocket = require('ws');
 var reHttpSignalhost = /^http(.*)$/;
 var reTrailingSlash = /\/$/;
+var pingers = [];
+var PINGHEADER = 'primus::ping::';
+var PONGHEADER = 'primus::pong::';
 
 function connect(signalhost) {
+  var socket;
+
   // if we have a http/https signalhost then do some replacement magic to push across
   // to ws implementation (also add the /primus endpoint)
   if (reHttpSignalhost.test(signalhost)) {
@@ -11,8 +16,33 @@ function connect(signalhost) {
       .replace(reTrailingSlash, '') + '/primus';
   }
 
-  return new WebSocket(signalhost);
+  socket = new WebSocket(signalhost);
+
+  socket.on('message', function(data) {
+    if (data.slice(0, PONGHEADER.length) === PONGHEADER) {
+      queuePing(socket);
+    }
+  });
+
+  queuePing(socket);
+  return socket;
 }
+
+function queuePing(socket) {
+  pingers.push(socket);
+}
+
+setInterval(function() {
+  pingers.splice(0).forEach(function(socket) {
+    if (socket.readyState === 1) {
+      socket.send(PINGHEADER + Date.now(), function(err) {
+        if (err) {
+          console.log('could not send ping: ', err);
+        }
+      });
+    }
+  });
+}, 10e3);
 
 module.exports = function(signalhost, opts, callback) {
   var ws = connect(signalhost);
