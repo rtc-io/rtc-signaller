35d454bf28f4a1d49ed8d126418fcb16e501316a
Damon Oehlman <damon.oehlman@gmail.com>
2014-10-02 15:44:49 +1000

    Remove files that will not be required in the future

--- main.js
+++
@@ -1,9 +0,0 @@
-var extend = require('cog/extend');
-
-module.exports = function(messenger, opts) {
-  return require('./signaller.js')(messenger, extend({
-    writeMethod: 'send',
-    dataEvent: 'message',
-    connect: require('./ws-connect')
-  }, opts));
-};
