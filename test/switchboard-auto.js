de915ed8a4ffe7c3d364a78f000cfc19864ef23c
Damon Oehlman <damon.oehlman@gmail.com>
2014-10-07 15:10:07 +1100

    Fix test where the event may trigger between tests

--- test/switchboard-auto.js
+++ test/switchboard-auto.js
@@ -10,14 +10,10 @@ test('can create a signalling instance that automatically connects via primus', 
 });
 
 test('can announce prior to the connection being established', function(t) {
-  t.plan(1);
+  t.plan(2);
   sig.announce({ name: 'Fred', room: uuid() });
-  t.pass('Announce called without error');
-});
-
-test('will receive an open event when channel is open', function(t) {
-  t.plan(1);
   sig.once('connected', t.pass.bind(t, 'signaller open'));
+  t.pass('Announce called without error');
 });
 
 test('close the signaller', function(t) {
