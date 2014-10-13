93b4668637b76968d742b272145d5c73c18cac7a
Damon Oehlman <damon.oehlman@gmail.com>
2014-10-13 11:25:16 +1100

    Bump version to 4.0.1

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var CLOSE_METHODS = ['close', 'end'];
 // initialise signaller metadata so we don't have to include the package.json
 // TODO: make this checkable with some kind of prepublish script
 var metadata = {
-  version: '4.0.0'
+  version: '4.0.1'
 };
 
 /**
