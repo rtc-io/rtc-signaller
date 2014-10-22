77c23e02f2a2111ec38532bfc17ac52214471d4b
Damon Oehlman <damon.oehlman@gmail.com>
2014-10-22 11:44:13 +1100

    Bump version to 3.1.1

--- signaller.js
+++ signaller.js
@@ -17,7 +17,7 @@ var CLOSE_METHODS = ['close', 'end'];
 // initialise signaller metadata so we don't have to include the package.json
 // TODO: make this checkable with some kind of prepublish script
 var metadata = {
-  version: '3.1.0'
+  version: '3.1.1'
 };
 
 /**
