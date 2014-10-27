07915cd4b27f4ace280b72a58b30858a0dff6bf5
Damon Oehlman <damon.oehlman@gmail.com>
2014-10-27 16:00:00 +1100

    Use rtc-switchboard-messenger instead of custom url hacks

--- test/helpers/signalling-server.js
+++ test/helpers/signalling-server.js
@@ -1,10 +1,3 @@
 var url = typeof window != 'undefined' ? location.origin : 'http://localhost:3000';
-var package = require('../../package.json');
-var semver = require('semver');
 
-// if we hare using a switchboard version <= 2 then append /primus
-if (semver.satisfies('2.0.0', package.devDependencies['rtc-switchboard'])) {
-  url += '/primus';
-}
-
-module.exports = require('messenger-ws')(url);
+module.exports = require('rtc-switchboard-messenger')(url);
