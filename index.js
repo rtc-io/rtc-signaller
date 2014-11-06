f4f7c3201b5842aa6f78115b780405d183ae5e70
Damon Oehlman <damon.oehlman@gmail.com>
2014-11-06 15:11:25 +1100

    Bump version to 5.2.2

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var RS_CONNECTED = 2;
 // initialise signaller metadata so we don't have to include the package.json
 // TODO: make this checkable with some kind of prepublish script
 var metadata = {
-  version: '5.2.1'
+  version: '5.2.2'
 };
 
 /**
