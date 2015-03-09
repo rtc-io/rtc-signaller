a4120ce98de99749c189e4471ce4e29ca3221c3b
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-09 21:31:44 +1100

    Renable disabled tests

--- test/all.js
+++ test/all.js
@@ -3,19 +3,19 @@ var createGroup = require('./helpers/messenger-group');
 // require('./to')(messenger, peers);
 
 module.exports = function(signalingServer) {
-//   require('./announce-event-local');
-//   require('./announce-concurrent');
-//   require('./announce-customid');
-//   require('./announce-debounce');
-//   require('./peer-filter');
-//   require('./set-id');
-//   require('./events');
-//   require('./to');
+  require('./announce-event-local');
+  require('./announce-concurrent');
+  require('./announce-customid');
+  require('./announce-debounce');
+  require('./peer-filter');
+  require('./set-id');
+  require('./events');
+  require('./to');
 
-//   // inspect generated messages
-//   require('./announce-raw')(createGroup(3));
-//   require('./send-falsey-parts')(createGroup(2));
-//   require('./message-announce')(createGroup(2));
+  // inspect generated messages
+  require('./announce-raw')(createGroup(3));
+  require('./send-falsey-parts')(createGroup(2));
+  require('./message-announce')(createGroup(2));
 
   // test automatic messenger implementation
   require('./switchboard-auto')(signalingServer);
