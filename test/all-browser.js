83d1e7877aab9fbbaf60f0167faea93a1c3c7c60
Damon Oehlman <damon.oehlman@gmail.com>
2015-04-27 21:03:27 +1000

    Use signaller-testrun for browser tests

--- test/all-browser.js
+++ test/all-browser.js
@@ -1,1 +1,9 @@
-require('./all')(location.origin);
+var signaller = require('..');
+var messenger = require('rtc-switchboard-messenger');
+
+function createSignaller(opts) {
+  return signaller(messenger(location.origin), opts);
+}
+
+// require('./all')(location.origin);
+require('rtc-signaller-testrun')(createSignaller);
